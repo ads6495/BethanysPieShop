9c1cad3d154a966082f166fd7e1b8a06480d6653
ads6495 <astrange55@icloud.com>
2019-10-08 23:44:14 -0400

    Revert "Revert "Models folder and services for MVC""
    This reverts commit c6154f120f437eb1bffe009980ffeb481fff35a8.

---
+++ BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository: IPieRepository
+    {
+        private readonly ICategoryRepository _ = new MockCategoryRepository();
+
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription="lorem ipsum", LongDescription="lorem ipsum"},
+                new Pie {PieId = 1, Name="Cheesecake", Price=19.95M, ShortDescription="lorem ipsum", LongDescription="lorem ipsum"},
+                new Pie {PieId = 1, Name="Rhubarb Pie", Price=15.95M, ShortDescription="lorem ipsum", LongDescription="lorem ipsum"},
+                new Pie {PieId = 1, Name="Pumpkin Pie", Price=12.95M, ShortDescription="lorem ipsum", LongDescription="lorem ipsum"}
+            };
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public Pie GetPieById(int PieId) 
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == PieId);
+        }
+    }
+}
